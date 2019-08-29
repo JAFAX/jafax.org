58f9b83c9426ae95dbcb4cbf76757a1243b031c8
Gary Greene <greeneg@tolharadys.net>
2019-08-28 23:36:55 -0400

    Use link.shlib and correct command substitution
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- scripts/setup_apache.sh
+++ scripts/setup_apache.sh
@@ -5,13 +5,14 @@ set -u
 set -o pipefail
 
 INSTALL_ROOT=/srv/buyo
+. ${INSTALL_ROOT}/scripts/lib/link.shlib
 
 # make sure we have the latest version of the buyo framework checked out
 cd ${INSTALL_ROOT}
 git pull
 
 # link our apache site config into place
-ln -s ${INSTALL_ROOT}/apache2/site/${hostname --fqdn}.conf /etc/apache2/sites-enabled/100-buyo.conf
+softlink ${INSTALL_ROOT}/apache2/site/$(hostname -f).conf /etc/apache2/sites-enabled/100-buyo.conf
 
 # enable the modules we need
 for MOD in 'proxy' 'proxy_http' 'proxy_balancer' 'lbmethod_byrequests'; do
