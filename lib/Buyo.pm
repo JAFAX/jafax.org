4f9e53fa62b43890e791e703f08fda90b42a7c94
Gary Greene <greeneg@tolharadys.net>
2019-09-02 20:53:01 -0400

    Dynamic route configuration from api.json
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- lib/Buyo.pm
+++ lib/Buyo.pm
@@ -15,31 +15,65 @@ use Dancer2;
 use JSON qw();
 use Data::Dumper;
 
-use Buyo::Constants;
-use Buyo::Utils qw(err_log);
+use FindBin;
+use lib "$FindBin::Bin/../lib";
 
-our $VERSION = $Buyo::Constants::version;
-my $DEBUG = true;
+use Dancer2::Swagger;
+
+use Buyo::Constants;
+use Buyo::Utils;
+
+sub get_json {
+    my ($config, $json_file) = @_;
+
+    err_log("== DEBUGGING ==: APPDIR: $config->{'appdir'}") if $config->{'debug'};
+    err_log("== DEBUGGING ==: JSON FILE: $json_file") if $config->{'debug'};
+
+    my $json_txt;
+    {
+        local $INPUT_RECORD_SEPARATOR;
+        open my $fh, '<', "$config->{'appdir'}/$json_file" or
+          croak "Unable to open $json_file";
+        $json_txt = <$fh>;
+        close $fh;
+    }
+
+    return $json_txt;
+}
+
+sub register_get_routes {
+    my ($config, @paths) = @_;
+
+    my $sub = (caller(0))[3];
+    err_log("== DEBUGGING ==: Sub: $sub") if $config->{'debug'};
+
+    foreach my $path (@paths) {
+        err_log("== DEBUGGING ==: Registering GET action for path $path") if $config->{'debug'};
+        get "$path" => sub {
+            err_log("== DEBUGGING ==: Triggering GET action for path $path") if $config->{'debug'};
+            template "index";
+        };
+    }
+
+    return true;
+}
+
+my $VERSION = $Buyo::Constants::version;
+my $DEBUG   = true;
 
 set traces  => 1;
 
-# Dancer2 configuration
-my $appdir = config->{appdir};
-#my %config = load_config($appdir);
+my %app_config = (
+    'appdir' => config->{appdir},
+    'debug'  => $DEBUG
+);
 
-say "Dancer2 configuration";
-say Dumper config;
+my $swagger = Dancer2::Swagger->new($DEBUG);
 
 my @getters;
 my @posters;
-my $json_txt;
-{
-    local $INPUT_RECORD_SEPARATOR;
-    open my $fh, '<', "$appdir/api.json" or
-      croak "Unable to open api.json";
-    $json_txt = <$fh>;
-    close $fh;
-}
+
+my $json_txt = get_json(\%app_config, 'api.json');
 my $json = JSON->new();
 my $data = $json->decode($json_txt);
 my %paths = %{$data->{'paths'}};
@@ -53,17 +87,7 @@ foreach my $path (keys %paths) {
         push @posters, $path;
     }
 }
-#
-#foreach my $path (@getters) {
-#    err_log("== DEBUGGING ==: Registering GET action for path $path") if $DEBUG;
-#    get "$path" => sub {
-#        err_log("== DEBUGGING ==: Triggering GET action for path $path") if $DEBUG;
-#        template "index";
-#    };
-#}
-#
-get '/' => sub {
-    template "index";
-};
 
-1;
+register_get_routes(\%app_config, @getters);
+
+true;
