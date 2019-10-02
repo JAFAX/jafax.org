0bf6f85fa90b3e6f715b49451f020fe0ec95db8f
Gary Greene <greeneg@tolharadys.net>
2019-10-01 23:29:59 -0400

    Changes:
    - Add license header to perl sources
    - Set copyright to JAFAX, Inc
    - Ensure that register_get_routes uses the right template
    
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- lib/Buyo.pm
+++ lib/Buyo.pm
@@ -1,3 +1,21 @@
+#
+# Author: Gary Greene <greeneg@tolharadys.net>
+# Copyright: 2019 JAFAX, Inc. All Rights Reserved
+#
+##########################################################################
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 package Buyo;
 
 use strict;
@@ -61,14 +79,19 @@ sub get_json {
 sub register_get_routes {
     my ($config, $bindings, @paths) = @_;
 
+    # un-reference to make easier to work with
+    my %bindings = %$bindings;
+
     my $sub = (caller(0))[3];
     err_log("== DEBUGGING ==: Sub: $sub") if $config->{'debug'};
 
     foreach my $path (@paths) {
+        my $template = $bindings{$path}->{'get'}->{'template'};
         err_log("== DEBUGGING ==: Registering GET action for path $path") if $config->{'debug'};
+        err_log("== DEBUGGING ==: Using template $template for path $path") if $config->{'debug'};
         get "$path" => sub {
             err_log("== DEBUGGING ==: Triggering GET action for path $path") if $config->{'debug'};
-            template "index";
+            template $template;
         };
     }
 
