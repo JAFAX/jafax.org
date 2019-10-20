8019ef6d155e1c626ddd362d204c44ca349edf0f
Gary Greene <greeneg@tolharadys.net>
2019-10-20 17:35:03 -0400

    More work on dynamic articles
    - Some technical debt being used by utilizing JSON files instead
      of a DB or key/value server, so we can get this done by deadline
    - Template in repo
    
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- lib/Buyo.pm
+++ lib/Buyo.pm
@@ -19,13 +19,12 @@
 package Buyo;
 
 use strict;
-use warnings;
+use warnings FATAL => 'all';
 use English qw(-no_match_vars);
 use utf8;
 
-use feature qw(
-    say
-);
+use feature qw(:5.26);
+no warnings "experimental::smartmatch";
 
 use boolean qw(:all);
 use CGI::Carp qw(carp fatalsToBrowser);
@@ -53,6 +52,7 @@ sub load_config {
     $configuration{'debug'}           = $config->val('General', 'debugging');
     $configuration{'webroot'}         = $config->val('Web', 'webpath');
     $configuration{'session_support'} = $config->val('Web', 'session_support', 0);
+    $configuration{'article_mech'}    = $config->val('Web', 'article_mech', "JSON");
     $configuration{'etcd_user'}       = $config->val('etcd', 'user');
     $configuration{'etcd_password'}   = $config->val('etcd', 'pass');
 
@@ -61,6 +61,9 @@ sub load_config {
 
 sub get_json {
     my ($config, $json_file) = @_;
+
+    my $sub = (caller(0))[3];
+    err_log("== DEBUGGING ==: Sub: $sub") if $config->{'debug'};
 
     err_log("== DEBUGGING ==: APPDIR: $config->{'appdir'}") if $config->{'debug'};
     err_log("== DEBUGGING ==: JSON FILE: $json_file") if $config->{'debug'};
@@ -77,6 +80,118 @@ sub get_json {
     return $json_txt;
 }
 
+sub register_static_route {
+    my ($verb, $config, $bindings, $path) = @_;
+
+    # un-reference to make easier to work with
+    my %bindings = %$bindings;
+
+    my $sub = (caller(0))[3];
+    err_log("== DEBUGGING ==: Sub: $sub") if $config->{'debug'};
+
+    my $template = $bindings{$path}->{'get'}->{'template'};
+    err_log("== DEBUGGING ==: Registering GET action for path '$path'") if $config->{'debug'};
+    err_log("== DEBUGGING ==: Using template '$template' for path '$path'") if $config->{'debug'};
+    given ($verb) {
+        when ('get') {
+            get "$path" => sub {
+                err_log("== DEBUGGING ==: Triggering GET action for path $path") if $config->{'debug'};
+                template $template, {
+                    'webroot'    => $config->{'webroot'},
+                    'site_name'  => $config->{'site_title'},
+                    'page_title' => $bindings->{$path}->{'get'}->{'summary'},
+                    'copyright'  => $config->{'copyright'},
+                    'license'    => $config->{'license'}
+                };
+            };
+        }
+        when ('put') {
+
+        }
+    }
+
+    return true;
+}
+
+sub get_article_from_json {
+    my ($config, $article) = @_;
+
+    my $sub = (caller(0))[3];
+    err_log("== DEBUGGING ==: Sub: $sub") if $config->{'debug'};
+
+    my $json_txt       = get_json($config, "content/${article}.json");
+    my $json           = JSON->new();
+    my $article_struct = $json->decode($json_txt);
+
+    my $author   = $article_struct->{'info'}->{'author'};
+    my $category = $article_struct->{'info'}->{'category'};
+    my $date     = $article_struct->{'info'}->{'date'};
+    my $title    = $article_struct->{'title'};
+    my $content  = $article_struct->{'content'};
+
+    return $author, $category, $date, $title, $content;
+}
+
+sub register_dynamic_route {
+    my ($verb, $config, $bindings, $path) = @_;
+
+    # un-reference to make easier to work with
+    my %bindings = %$bindings;
+
+    my $sub = (caller(0))[3];
+    err_log("== DEBUGGING ==: Sub: $sub") if $config->{'debug'};
+
+    my $article_author;
+    my $article_category;
+    my $article_content;
+    my $article_date;
+    my $article_title;
+
+    my $template     = lc($bindings{$path}->{'get'}->{'template'});
+    my $article_mech = uc($config->{'configuration'}->{'article_mech'});
+    err_log("== DEBUGGING ==: Using Article Content Mechanism '$article_mech'") if $config->{'debug'};
+    err_log("== DEBUGGING ==: Registering GET action for path '$path'") if $config->{'debug'};
+    err_log("== DEBUGGING ==: Using template '$template' for path '$path'") if $config->{'debug'};
+    given ($verb) {
+        when ('get') {
+            get "$path" => sub {
+                my $article  = route_parameters->get('article');
+                err_log("== DEBUGGING ==: Triggering GET action for path '$path'") if $config->{'debug'};
+                err_log("== DEBUGGING ==: Generating page for article '$article'") if $config->{'debug'};
+                # gather up article information
+                # get article title
+                given ($article_mech) {
+                    when ('JSON') {
+                        ($article_author, $article_category, $article_date,
+                            $article_title, $article_content) = get_article_from_json($config, $article);
+                        break;
+                    }
+                    default {
+                        err_log("== WARNING ==: Unknown Article Content Mechanism, '$article_mech'");
+                    }
+                }
+                template $template, {
+                    'webroot'       => $config->{'webroot'},
+                    'site_name'     => $config->{'site_title'},
+                    'page_title'    => $bindings->{$path}->{'get'}->{'summary'},
+                    'copyright'     => $config->{'copyright'},
+                    'license'       => $config->{'license'},
+                    'author'        => $article_author,
+                    'category'      => $article_category,
+                    'date'          => $article_date,
+                    'title'         => $article_title,
+                    'page_content'  => $article_content
+                };
+            };
+        }
+        when ('put') {
+
+        }
+    }
+    
+    return true;
+}
+
 sub register_get_routes {
     my ($config, $bindings, @paths) = @_;
 
@@ -87,26 +202,20 @@ sub register_get_routes {
     err_log("== DEBUGGING ==: Sub: $sub") if $config->{'debug'};
 
     foreach my $path (@paths) {
-        my $template = $bindings{$path}->{'get'}->{'template'};
-        err_log("== DEBUGGING ==: Registering GET action for path $path") if $config->{'debug'};
-        err_log("== DEBUGGING ==: Using template $template for path $path") if $config->{'debug'};
-        get "$path" => sub {
-            err_log("== DEBUGGING ==: Triggering GET action for path $path") if $config->{'debug'};
-            if (defined($bindings{$path}->{'get'}->{'subroutine'})) {
-                my $cleaned_path = $path;
-                $cleaned_path =~ tr/\//_/; # get rid of slashes in path
-                my $call_name = "get$cleaned_path";
-                my $code = $bindings{$path}->{'get'}->{'subroutine'};
-                my $sub = eval "sub $call_name { $code }";
+        my $type = $bindings{$path}->{'get'}->{'type'};
+        if (defined($type)) {
+            err_log("== DEBUGGING ==: Path '$path' has type: '$type'") if $config->{'debug'};
+        } else {
+            err_log("== DEBUGGING ==: Path '$path' has no defined type!") if $config->{'debug'};
+        }
+        given ($type) {
+            when ('dynamic') {
+                register_dynamic_route('get', $config, $bindings, $path);
             }
-            template $template, {
-                'webroot'    => $config->{'webroot'},
-                'site_name'  => $config->{'site_title'},
-                'page_title' => $bindings->{$path}->{'get'}->{'summary'},
-                'copyright'  => $config->{'copyright'},
-                'license'    => $config->{'license'}
-            };
-        };
+            when ('static') {
+                register_static_route('get', $config, $bindings, $path);
+            }
+        }
     }
 
     return true;
@@ -119,9 +228,9 @@ sub register_post_routes {
     err_log("== DEBUGGING ==: Sub: $sub") if $config->{'debug'};
 
     foreach my $path (@paths) {
-        err_log("== DEBUGGING ==: Registering POST action for path $path") if $config->{'debug'};
+        err_log("== DEBUGGING ==: Registering POST action for path '$path'") if $config->{'debug'};
         post "$path" => sub {
-            err_log("== DEBUGGING ==: Triggering POST action for path $path") if $config->{'debug'};
+            err_log("== DEBUGGING ==: Triggering POST action for path '$path'") if $config->{'debug'};
 
         };
     }
@@ -146,6 +255,7 @@ sub main {
     my $data = $json->decode($json_txt);
     my %app_config = (
         'appdir'        => config->{appdir},
+        'article_mech'  => $configuration{'article_mech'},
         'debug'         => $configuration{'debug'},
         'configuration' => \%configuration,
         'site_title'    => $data->{'info'}->{'title'},
