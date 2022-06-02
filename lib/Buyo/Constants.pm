2ee390aea2899ef3b519cd6792670200e16fd673
Gary Greene <greeneg@tolharadys.net>
2022-06-01 21:26:27 -0400

    Bump version number
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- lib/Buyo/Constants.pm
+++ lib/Buyo/Constants.pm
@@ -25,7 +25,7 @@ package Buyo::Constants {
     use boolean qw(:all);
     use base qw(Exporter);
 
-    our $VERSION = "1.2.61";
+    our $VERSION = "1.2.62";
 
     BEGIN {
         use Exporter   ();
