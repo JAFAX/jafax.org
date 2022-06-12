5e774dd9b60b05e51025e9325cbf1c254c884467
Gary Greene <greeneg@tolharadys.net>
2022-06-11 21:11:08 -0400

    Announce Charles and Samurai Dan and more
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- lib/Buyo/Constants.pm
+++ lib/Buyo/Constants.pm
@@ -25,7 +25,7 @@ package Buyo::Constants {
     use boolean qw(:all);
     use base qw(Exporter);
 
-    our $VERSION = "1.2.64";
+    our $VERSION = "1.2.65";
 
     BEGIN {
         use Exporter   ();
