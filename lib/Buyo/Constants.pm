a0c74700bdb2a757a9defe7ab6369772a341fba2
Gary Greene <greeneg@tolharadys.net>
2022-06-12 20:28:11 -0400

    Misspelt name
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- lib/Buyo/Constants.pm
+++ lib/Buyo/Constants.pm
@@ -25,7 +25,7 @@ package Buyo::Constants {
     use boolean qw(:all);
     use base qw(Exporter);
 
-    our $VERSION = "1.2.66";
+    our $VERSION = "1.2.67";
 
     BEGIN {
         use Exporter   ();
