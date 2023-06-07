95602e82c8d2d85f66ada111a81a1a44db7a4578
Gary Greene <greeneg@tolharadys.net>
2023-06-07 18:12:23 -0400

    Update schedule page to use embedded doc
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- lib/Buyo/Constants.pm
+++ lib/Buyo/Constants.pm
@@ -25,7 +25,7 @@ package Buyo::Constants {
     use boolean qw(:all);
     use base qw(Exporter);
 
-    our $VERSION = "1.2.110";
+    our $VERSION = "1.2.111";
 
     BEGIN {
         use Exporter   ();
